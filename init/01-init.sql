77835d572964f936619aba9fd30ac4df8596c8a4
oyavri <oyavri@gmail.com>
2025-10-18 17:54:55 +0300

    Fix naming of columns of tables

--- init/01-init.sql
+++ init/01-init.sql
@@ -8,9 +8,9 @@ END$$;
 CREATE TABLE IF NOT EXISTS "User" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT UNIQUE NOT NULL,
-    fullname TEXT NOT NULL,
-    createdAt TIMESTAMP NOT NULL DEFAULT NOW(),
-    passwordHash TEXT NOT NULL,
+    full_name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    password_hash TEXT NOT NULL,
     salt TEXT NOT NULL
 );
 
@@ -18,15 +18,15 @@ CREATE TABLE IF NOT EXISTS "Call" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     title TEXT NOT NULL,
     duration INT NOT NULL, 
-    createdAt TIMESTAMP NOT NULL DEFAULT NOW(),
-    createdBy UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_by UUID NOT NULL,
     active BOOLEAN NOT NULL,
     CONSTRAINT fk_call_user FOREIGN KEY (createdBy) REFERENCES "User"(id)
 );
 
 CREATE TABLE IF NOT EXISTS "Participants" (
-    callId UUID NOT NULL,
-    userId UUID NOT NULL,
+    call_id UUID NOT NULL,
+    user_id UUID NOT NULL,
     PRIMARY KEY (callId, userId),
     CONSTRAINT fk_participants_call FOREIGN KEY (callId) REFERENCES "Call"(id),
     CONSTRAINT fk_participants_user FOREIGN KEY (userId) REFERENCES "User"(id)
@@ -34,9 +34,9 @@ CREATE TABLE IF NOT EXISTS "Participants" (
 
 CREATE TABLE IF NOT EXISTS "Transcription" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    callId UUID UNIQUE NOT NULL,
+    call_id UUID UNIQUE NOT NULL,
     status transcription_status NOT NULL,
     transcription TEXT,
-    initiatedAt TIMESTAMP NOT NULL,
+    initiated_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_transcription_call FOREIGN KEY (callId) REFERENCES "Call"(id)
 );
