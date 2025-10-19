55b98c0c905e6b9b9dcf6ce24dac7357836352a4
oyavri <oyavri@gmail.com>
2025-10-19 17:00:04 +0300

    Introduce endpoints for call actions

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import { logger } from './common/logger.js';
 import { validateLogin, validateRegister } from './validators/userValidator.js';
 import { userController } from './controllers/userController.js';
 import { authenticate } from './middlewares/authentication.js';
+import { validateCall } from './validators/callValidator.js';
+import { callController } from './controllers/callController.js';
 
 const app = express();
 app.use(express.json());
@@ -12,11 +14,11 @@ app.use(express.json());
 app.post('/auth/register', validateRegister, userController.register);
 app.post('/auth/login', validateLogin, userController.login);
 app.get('/auth/me', authenticate, userController.getMe);
-// app.post('/calls');
-// app.get('/calls');
-// app.get('/calls/:id');
-// app.delete('/calls/:id');
-// app.get('/calls/:id/transcription');
+app.post('/calls', validateCall, authenticate, callController.createCall);
+app.get('/calls', authenticate, callController.getCalls);
+app.get('/calls/:id', authenticate, callController.getCallDetails);
+app.delete('/calls/:id', authenticate, callController.deleteCall);
+app.get('/calls/:id/transcription', authenticate, callController.getCallTranscription);
 
 app.get('/health', async (req, res) => {
     res.status(200).send('OK');
