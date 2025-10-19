e4b32d92fe29481a503ac58b1011dd9e978ebe54
oyavri <oyavri@gmail.com>
2025-10-19 19:53:30 +0300

    Change logging format of the logger

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,7 +1,24 @@
 import winston from 'winston';
 
+const { combine, timestamp, json, printf } = winston.format;
+const timestampFormat = 'DD-MM-YYYY HH:mm:ss';
+
 export const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports: [new winston.transports.Console()],
+    format: combine(
+        timestamp({ format: timestampFormat }),
+        json(),
+        printf(({ timestamp, level, message, ...data }) => {
+            const response = {
+                level,
+                timestamp,
+                message,
+                data,
+            };
+
+            return JSON.stringify(response);
+        }),
+    ),
+    transports: [
+        new winston.transports.Console()
+    ],
 });
