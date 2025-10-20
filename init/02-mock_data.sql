afe2176e785f02ccc661e8c84cd0153cc431543c
oyavri <oyavri@gmail.com>
2025-10-20 14:19:40 +0300

    Add mock data for user

--- init/02-mock_data.sql
+++ init/02-mock_data.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (id, email, full_name, password_hash)
 VALUES ('83d717c6-e774-4be1-8883-e2709d6c62c2', 'thomas.anderson@matrix.com', 'Thomas A. Anderson', '$2b$10$qU.nxKqkUDMsCv4bo50CCOKTQNCJDZrcc819R0c5.A4Zq6a/UBF2W');
+INSERT INTO users (id, email, full_name, password_hash)
+VALUES ('8fa3a9d2-649c-4130-80df-513f18f63253', 'onur@test.com', 'Onur', '2b$10$PCnDCSgGzD5Ppz0IDj6aTenVXaTXA5/rnxSH8k/ZHOfKpbcy1kF7e')
